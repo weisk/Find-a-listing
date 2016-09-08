5e282932ed313571e4c470c577e6430d18626330
Dom Talbot <domjt@me.com>
2016-09-08 22:01:24 +0100

    Refactored webpack route & added CSS filepaths

--- routes/webpack.js
+++ routes/webpack.js
@@ -1,7 +1,9 @@
-
 /* eslint-disable no-console */
 
 import { readFileSync } from 'fs';
+/* eslint-disable no-unused-vars */
+import { dim } from 'colors';
+/* eslint-enable no-unused-vars */
 
 import '../models/env';
 /* eslint-disable import/no-extraneous-dependencies, import/imports-first, import/no-unresolved */
@@ -17,6 +19,7 @@ export default function webpack(app) {
     let appJs;
     let vendorJs;
     let commonJs;
+    let appCss;
     if (process.env.NODE_ENV === 'production') {
         let manifestContents = readFileSync(`./public${publicPath}manifest.json`);
         manifestContents = JSON.parse(manifestContents);
@@ -24,19 +27,22 @@ export default function webpack(app) {
         app.locals.webpack_manifest = JSON.stringify(manifestContents);
         app.locals.GA_ID = process.env.GA_ID;
         /* eslint-enable no-param-reassign */
-        vendorJs = assetsByChunkName.vendor;
-        appJs = assetsByChunkName.app;
-        commonJs = assetsByChunkName.common;
+        vendorJs = assetsByChunkName.vendor[0];
+        appJs = assetsByChunkName.app[0];
+        appCss = assetsByChunkName.app[1];
+        commonJs = assetsByChunkName.common[0];
     } else {
         vendorJs = assetsByChunkName.vendor[0];
         appJs = assetsByChunkName.app[0];
+        appCss = assetsByChunkName.app[1];
         commonJs = assetsByChunkName.common[0];
     }
     /* eslint-disable no-param-reassign */
     app.locals.webpack_vendor = `${publicPath}${vendorJs}`;
     app.locals.webpack_app = `${publicPath}${appJs}`;
+    app.locals.webpack_app_css = `${publicPath}${appCss}`;
     app.locals.webpack_common = `${publicPath}${commonJs}`;
     /* eslint-enable no-param-reassign */
 
-    console.log(`Asset path: ${publicPath}`);
+    console.log('Asset path:'.dim, publicPath);
 }
