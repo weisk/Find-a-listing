ed19f88bac81eebcd7513f50d8bea4104e17a0b7
Dom Talbot <domjt@me.com>
2016-09-08 23:02:22 +0100

    Tweaked basic react-map-gl & added decorators

--- modules/Map/index.js
+++ modules/Map/index.js
@@ -1,14 +1,22 @@
 import React, { Component, PropTypes } from 'react';
+import autobind from 'autobind-decorator';
 import MapGL from 'react-map-gl';
 
 export default class Map extends Component {
     static displayName = 'Map';
 
     static defaultProps = {
+        width: 250,
+        height: 250,
+        mapStyle: 'mapbox://styles/mapbox/satellite-v9',
         mapboxApiAccessToken: process.env.mapboxKey,
     }
 
     static propTypes = {
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+        mapStyle: PropTypes.string.isRequired,
+        onChangeViewport: PropTypes.func,
         mapboxApiAccessToken: PropTypes.string.isRequired,
     }
 
@@ -20,11 +28,23 @@ export default class Map extends Component {
         },
     }
 
+    @autobind
+    onChangeViewport(viewport) {
+        if (this.props.onChangeViewport) {
+            return this.props.onChangeViewport(viewport);
+        }
+        return this.setState({ viewport });
+    }
+
     render() {
+        const mapProps = {
+            ...this.state.viewport,
+            ...this.props,
+            onChangeViewport: this.onChangeViewport,
+        };
         return (
             <MapGL
-              {...this.state.viewport}
-              {...this.props}
+              {...mapProps}
             />
         );
     }
