e47496178157420dac55d2fee532dbc105f8236a
Dom Talbot <domjt@me.com>
2016-09-08 21:57:09 +0100

    Refactored webpack.server.config
    Added Scss support
    Dedupe

--- webpack.server.config.babel.js
+++ webpack.server.config.babel.js
@@ -1,9 +1,14 @@
 import { writeFileSync } from 'fs';
 import { resolve, join } from 'path';
 
+import ExtractTextPlugin, { extract } from 'extract-text-webpack-plugin';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
 import node_modules from 'webpack-node-externals';
-import { optimize } from 'webpack';
+import { optimize, DefinePlugin } from 'webpack';
+
+import './models/env';
+
+const NODE_ENV = process.env.NODE_ENV;
 
 export default {
     context: __dirname,
@@ -16,10 +21,17 @@ export default {
     },
     target: 'node',
     node: {
-            __filename: false,
-            __dirname: true,
+        __filename: false,
+        __dirname: true,
     },
     resolve: {
+        extensions: [
+            '',
+            '.js',
+            '.scss',
+            '.sass',
+            '.css',
+        ],
         alias: {
             'webpackjson': '/bin/webpack.json',
         },
@@ -44,12 +56,28 @@ export default {
                 exclude: /node_modules/,
                 loader: 'babel',
             },
+            {
+                test: /\.scss$/,
+                exclude: ['node_modules'],
+                loader: extract([
+                    'css?-minimize',
+                    'sass',
+                ]),
+            },
         ]
     },
     eslint: {
         fix: true,
         failOnError: false,
         failOnWarning: false,
+    },
+    sassLoader: {
+        includePaths: [
+            __dirname,
+            resolve(__dirname, 'node_modules'),
+            resolve(__dirname, 'assets/scss'),
+            resolve(__dirname, 'assets/scss/components'),
+        ],
     },
     plugins: [
         new CleanWebpackPlugin([
@@ -58,12 +86,16 @@ export default {
         ], {
               root: __dirname,
         }),
+        new ExtractTextPlugin({
+            filename: '[name].css',
+        }),
         new optimize.UglifyJsPlugin({
             compress: {
                 warnings: false,
             },
             comments: false,
         }),
+        new optimize.DedupePlugin(),
         new optimize.OccurrenceOrderPlugin(),
         function() {
             this.plugin('done', stats => {
