a881d61b569e0887b85e049040e9edcb02240e48
Dom Talbot <domjt@me.com>
2016-09-10 16:59:04 +0100

    Fixed production webpack paths & added Babel plugins and presets

--- routes/webpack.js
+++ routes/webpack.js
@@ -2,7 +2,7 @@
 
 import { readFileSync } from 'fs';
 /* eslint-disable no-unused-vars */
-import { dim } from 'colors';
+import { dim, magenta } from 'colors';
 /* eslint-enable no-unused-vars */
 
 import '../models/env';
@@ -16,6 +16,7 @@ import { assetsByChunkName, publicPath } from 'webpackjson';
  * @return {null} Sets Express variables
  */
 export default function webpack(app) {
+    const webpackPaths = {};
     let appJs;
     let vendorJs;
     let commonJs;
@@ -27,22 +28,29 @@ export default function webpack(app) {
         app.locals.webpack_manifest = JSON.stringify(manifestContents);
         app.locals.GA_ID = process.env.GA_ID;
         /* eslint-enable no-param-reassign */
-        vendorJs = assetsByChunkName.vendor[0];
+        vendorJs = assetsByChunkName.vendor;
         appJs = assetsByChunkName.app[0];
         appCss = assetsByChunkName.app[1];
-        commonJs = assetsByChunkName.common[0];
+        commonJs = assetsByChunkName.common;
     } else {
         vendorJs = assetsByChunkName.vendor[0];
         appJs = assetsByChunkName.app[0];
         appCss = assetsByChunkName.app[1];
         commonJs = assetsByChunkName.common[0];
     }
+    webpackPaths.vendor = `${publicPath}${vendorJs}`;
+    webpackPaths.app = `${publicPath}${appJs}`;
+    webpackPaths.app_css = `${publicPath}${appCss}`;
+    webpackPaths.common = `${publicPath}${commonJs}`;
     /* eslint-disable no-param-reassign */
-    app.locals.webpack_vendor = `${publicPath}${vendorJs}`;
-    app.locals.webpack_app = `${publicPath}${appJs}`;
-    app.locals.webpack_app_css = `${publicPath}${appCss}`;
-    app.locals.webpack_common = `${publicPath}${commonJs}`;
+    app.locals.webpack = webpackPaths;
     /* eslint-enable no-param-reassign */
 
-    console.log('Asset path:'.dim, publicPath);
+    console.log('Asset:'.dim, publicPath);
+    const webpackPathsNames = Object.keys(webpackPaths);
+    const webpackPathsValues = Object.values(webpackPaths);
+    webpackPathsValues.forEach((webpackPath, index) => {
+        const fileName = webpackPath.replace(publicPath, '');
+        console.log(`   ${webpackPathsNames[index]}:`.dim, `${fileName}`.magenta);
+    });
 }
